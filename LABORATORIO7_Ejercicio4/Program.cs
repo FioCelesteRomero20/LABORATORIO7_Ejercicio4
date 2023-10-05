5edb007d9d74ad332fa3492a651f3cf0f5b0500c
PC <PC@DESKTOP-NOE8GP1>
2023-10-04 19:02:27 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO7_Ejercicio4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO7_Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduzca un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write("Ingrese un número: ");
+                int num = int.Parse(Console.ReadLine());
+                for (int j = 0; j < num; j++)
+                {
+                    
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    } 
+}
